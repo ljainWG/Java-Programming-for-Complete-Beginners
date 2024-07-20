f567fc6269f5100ff566977990bb73ac19bb36b5
ljainWG <Lakshay.Jain@watchguard.com>
2024-07-20 09:31:44 +0530

    Created Second Java program to print table of a number

---
+++ Section 6/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package com.in28min.FirstJavaProject;
+
+public class MultiplicationTable {
+	
+	void printTable() {
+		printTable(2);
+	}
+	
+	void printTable(int num) {
+		int i = 0;
+		while(i++<10) {
+			System.out.printf("%d * %d = %d",num,i,num*i).println();
+		}
+	}
+	
+}
---
+++ Section 6/MultiplicationTableRunner.java
@@ -0,0 +1,11 @@
+package com.in28min.FirstJavaProject;
+
+public class MultiplicationTableRunner {
+	
+		public static void main(String[] args) {
+			MultiplicationTable table = new MultiplicationTable();
+			table.printTable();
+			table.printTable(7);
+		}
+	
+}
